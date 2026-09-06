a481058f9fa6c26b1955e278508e019840b03dff
Tanya <tanyaa1601@gmail.com>
2026-09-06 23:36:51 +0530

    feat(array): update brute force approach of two sum problem

--- arrays/two_sum_leetcode1/1_two_sum_brute_force.py
+++ arrays/two_sum_leetcode1/1_two_sum_brute_force.py
@@ -1,20 +1,20 @@
+'''
+Time Complexity: O(N2) because we use two nested loops to check every possible pair of elements in the array, where N is the size of the array.
 
+Space Complexity: O(1) as we use a constant amount of extra space regardless of input size.
+'''
 
 def twoSum(nums: list[int], target) -> int:
 
-    start,end = -1,-1
-
     for i in range(len(nums)-1):
-        start = i
         for j in range(i+1,len(nums)):
             if nums[j]+nums[i] == target:
-                end=j
                 return [i,j]
     return [-1,-1]
 
 
 nums = list(map(int, input("Enter the elements of the array (space-separated): ").split()))
-
+target = int(input("Enter the target value: "))
 indices = twoSum(nums, target)
 
-print("The indices for trget sum is:", indices)
+print("The indices for target sum is:", indices)
